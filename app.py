8b95ae7f44c0210f54ab7132064fb97b505600b9
Pranesh17-s <praneshpalamedu17@gmail.com>
2025-02-21 22:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,11 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+    try:
+        asyncio.get_running_loop()
+    except RuntimeError:
+        asyncio.run(main())  # Run normally if no event loop is running
+    else:
+        loop = asyncio.get_event_loop()
+        loop.create_task(main())
